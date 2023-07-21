247af7b615a42a085cb68ac7f5407afa345ae80b
maismohammed <mais.mohammed1994@gmail.com>
2023-07-21 20:01:00 +0300

    First React Task

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h2>I am Mais Mohammed</h2>
+            <p>I've completed the required task!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Components/Career.js
@@ -0,0 +1,10 @@
+function Career() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h2>Career</h2>
+            <p>I currently work as an IT engineer in a bank.</p>
+        </div>
+    );
+};
+
+export default Career;
